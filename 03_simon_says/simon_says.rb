81a5e5a797d5b56cc0619c7ef75c8a60f816b72c
dklounge <daviddarden11@gmail.com>
2013-03-31 14:24:17 -0700

    simon says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -16,4 +16,24 @@ def start_of_word(word, characters)
 end
 
 def first_word(sentence)
+  sentence_array = sentence.split
+  sentence_array.first
 end
+
+def titleize(sentence)
+  sentence_array = sentence.split
+  little_words = ["this","that", "and", "the", "a", "or", "from", "with", "unto", "is", "over"] # add other prepositions and articles as needed
+  sentence_array.each do |word|
+    if little_words.include? word
+      word
+    else
+      word.capitalize!
+    end
+  end
+
+  sentence_array[0].capitalize!
+  sentence_array.join(' ')
+
+end
+
+p titleize("the and this is no joke")
