e8b1cc0c699b780fead8b5624e004334e20bb620
dklounge <daviddarden11@gmail.com>
2013-04-29 19:06:24 -0700

    performance monitor

--- 06_performance_monitor/performance_monitor.rb
+++ 06_performance_monitor/performance_monitor.rb
@@ -1,5 +1,16 @@
 require 'time'
 
-def measure(time = nil)
-  yield
+def measure(n = 1)
+  elapsed_time = 0
+
+  n.times do 
+    start_time = Time.now
+
+    yield
+
+    end_time = Time.now
+    elapsed_time += end_time - start_time
+  end
+  
+  p elapsed_time / n
 end
