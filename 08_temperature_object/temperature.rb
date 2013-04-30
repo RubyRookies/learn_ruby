84359c6d2192046e7641159e694b402369628c06
dklounge <daviddarden11@gmail.com>
2013-04-29 23:14:16 -0700

    update to temp obj

--- 08_temperature_object/temperature.rb
+++ 08_temperature_object/temperature.rb
@@ -1,11 +1,31 @@
 class Temperature
- def in_fahrenheit
-  @degrees_fahrenheit
- end
+  attr_accessor :f, :c
+
+  def initialize args
+    args.each do |key, value|
+      instance_variable_set("@#{key}", value) unless value.nil?
+    end
+  end
+
+  def in_fahrenheit
+    f
+    f.ftoc
+  end
+
+  def in_celsius
+    c
+  end
+
+  def ftoc(f)
+    (f - 32.0) * (5.0/9.0)
+  end
+
+  def ctof(c)
+    c * (9.0/5.0) + 32
+  end
+
 end
 
-class Fahrenheit < Temperature
-  def initialize(f)
-    super(:f => f)
-  end
-end
+
+a = Temperature.new(:f => 50, :c => 0)
+p a.in_fahrenheit
